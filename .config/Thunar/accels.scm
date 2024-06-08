0c0427e17070a6b4619a7ffc8f100fe11e1d67b1
Lotrando <miroslav.klika@seznam.cz>
2024-06-08 23:58:17 +0200

    Update

--- .config/Thunar/accels.scm
+++ .config/Thunar/accels.scm
@@ -1,6 +1,7 @@
 ; thunar GtkAccelMap rc-file         -*- scheme -*-
 ; this file is an automated accelerator map dump
 ;
+; (gtk_accel_path "<Actions>/ThunarBookmarks/5c02d96417030b110f12d28a723d04cc" "")
 ; (gtk_accel_path "<Actions>/ThunarActionManager/restore" "")
 ; (gtk_accel_path "<Actions>/ThunarActionManager/undo" "<Primary>z")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/select-all-files" "<Primary>a")
@@ -81,6 +82,7 @@
 ; (gtk_accel_path "<Actions>/ThunarWindow/switch-previous-tab" "<Primary>Page_Up")
 ; (gtk_accel_path "<Actions>/ThunarWindow/zoom-in-alt1" "<Primary>KP_Add")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/toggle-sort-order" "")
+; (gtk_accel_path "<Actions>/ThunarBookmarks/30979c1c8984a9133df2494f27ff1370" "")
 ; (gtk_accel_path "<Actions>/ThunarActionManager/open-in-new-window" "<Primary><Shift>o")
 ; (gtk_accel_path "<Actions>/ThunarWindow/view-configure-toolbar" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/view-location-selector-menu" "")
--- README.md
+++
@@ -1,5 +0,0 @@
-# Realist's Hyprland Minimal Desktop [ RHMD ] on Gentoo Linux 
-
-## Config files (dotfiles)
-
-Contains complete config files for Gentoo linux with Hyperland desktop.
